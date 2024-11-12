f1627551021d66099ce74eb0b1a4a9005644c4b3
unknown <ryan53132@gmail.com>
2024-11-12 16:31:10 -0300

    fase de testes AWS Sqlite

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request
+import sqlite3
 
 app = Flask(__name__)   
 
@@ -8,3 +9,54 @@ def Home():
 @app.route("/quiz")
 def quiz():
     return render_template('test-scrum.html')
+
+@app.route('/Q-I', methods = ['POST','GET'] )
+def insert():
+    if request.method == 'POST':
+        try:
+            nm = request.form['nome']
+            cpf = request.form['CPF']
+            quiz = request.form['Q']
+            pts = request.form['pts']
+            with sqlite3.connect('database.db') as conn:
+                cur = conn.cursor()
+                cur.execute(f"SELECT * FROM users WHERE CPF = '{cpf}' AND Nome = '{nm}'")
+                if cur.fetchone():
+                    cur.execute(f"UPDATE users SET "+quiz+"="+pts+" WHERE CPF = '"+cpf+"'")
+                    conn.commit()
+                else:
+                    cur.execute(f"INSERT INTO users (CPF,Nome,{quiz}) values (?,?,?)", (cpf,nm,pts))
+                    conn.commit()
+        except:
+            conn.rollback()
+            msg ="Dados invalidos"
+        finally:
+            conn.close()
+            return render_template('test-scrum.html')
+@app.route('/delete', methods = ['POST','GET'])
+def delete():
+    if request.method == 'POST':
+        try:
+            nm = request.form['nome']
+            cpf = request.form['CPF']
+            quiz = request.form['Q']
+            pts = request.form['pts']
+            with sqlite3.connect('database.db') as conn:
+                cur = conn.cursor()
+                cur.execute("DELETE FROM users WHERE CPF = '"+cpf+"' and Nome = '"+nm+"'")
+                conn.commit()
+        except:
+            conn.rollback()
+            msg ="Dados invalidos"
+        finally:
+            conn.close()
+            return render_template('test-scrum.html')
+@app.route('/mostrar')
+def mostra():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("SELECT CPF, * FROM users")
+    rows = cur.fetchall()
+    cur.close()
+    return render_template('teste.html', rows = rows)
--- scheme.py
+++ scheme.py
@@ -1,6 +1,5 @@
 import sqlite3
 conn = sqlite3.connect('database.db')
-print('certo')
-conn.execute('CREATE TABLE Users (CPF TEXT,Nome TEXT,Q_I INTEGER,Q_II INTEGER,Q_III INTEGER,Q_IV INTEGER,Q_V INTEGER)')
-print('certo2')
+conn.execute("DROP TABLE users")
+conn.execute('CREATE TABLE Users (CPF TEXT NOT NULL PRIMARY KEY,Nome TEXT,quiz INTEGER,quizz INTEGER,quizzz INTEGER,quizzzz INTEGER,quizzzzz INTEGER)')
 conn.close()
